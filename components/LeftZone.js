ecb1c1b950decd6065ba1c182a79e7640680b565
Juan Curti <jmfcdev@gmail.com>
2023-04-14 15:14:56 +0200

    Menu added

--- components/LeftZone.js
+++ components/LeftZone.js
@@ -1,11 +1,29 @@
 import Head from 'next/head'
 
 export default function LeftZone() {
+
+  const menuButton = (assetName, title) => {
+    return <div className='menuButtonBox'>
+        <div className='menuImageContainer'>
+            <img src={assetName}/>
+            <p className='menuTitle'>{title}</p>
+        </div>
+    </div>
+  }
+
   return (
     <div className='leftZone'>
         <div className='twitterLogoBox'>
             <img src='/assets/twitter-icon.svg' className='twitterLogo'/>
         </div>
+        {menuButton('/assets/menu-home.svg', 'Home')}
+        {menuButton('/assets/menu-explore.svg', 'Explore')}
+        {menuButton('/assets/menu-notifications.svg', 'Notifications')}
+        {menuButton('/assets/menu-messages.svg', 'Messages')}
+        {menuButton('/assets/menu-bookmarks.svg', 'Bookmarks')}
+        {menuButton('/assets/menu-twitter-blue.svg', 'Twitter Blue')}
+        {menuButton('/assets/menu-profile.svg', 'Profile')}
+        {menuButton('/assets/menu-more.svg', 'More')}
     </div>
   )
 }
